d2bd14f5cc7dc965e51b0b49fb9d6ea95420ef64
Ping Liu <lpingbj@cn.ibm.com>
2025-07-01 22:05:26 +0100

    Add iceberg partition transforms.
    Co-authored-by: Chengcheng Jin <Chengcheng.Jin@ibm.com>
    
    Alchemy-item: (ID = 675) Iceberg staging hub commit 5/12 - 0acd427f38f3b0ae6e0e5f0edb8849900482303e

--- velox/connectors/hive/HivePartitionUtil.cpp
+++ velox/connectors/hive/HivePartitionUtil.cpp
@@ -79,11 +79,10 @@ std::pair<std::string, std::string> makePartitionKeyValueString(
     const BaseVector* partitionVector,
     vector_size_t row,
     const std::string& name,
-    bool isDate,
-    const std::string& nullValueName) {
+    bool isDate) {
   using T = typename TypeTraits<Kind>::NativeType;
   if (partitionVector->as<SimpleVector<T>>()->isNullAt(row)) {
-    return std::make_pair(name, nullValueName);
+    return std::make_pair(name, "");
   }
   if (isDate) {
     return std::make_pair(
@@ -117,8 +116,7 @@ std::pair<std::string, std::string> makePartitionKeyValueString(
 
 std::vector<std::pair<std::string, std::string>> extractPartitionKeyValues(
     const RowVectorPtr& partitionsVector,
-    vector_size_t row,
-    const std::string& nullValueName) {
+    vector_size_t row) {
   std::vector<std::pair<std::string, std::string>> partitionKeyValues;
   for (auto i = 0; i < partitionsVector->childrenSize(); i++) {
     partitionKeyValues.push_back(PARTITION_TYPE_DISPATCH(
@@ -127,8 +125,7 @@ std::vector<std::pair<std::string, std::string>> extractPartitionKeyValues(
         partitionsVector->childAt(i)->loadedVector(),
         row,
         asRowType(partitionsVector->type())->nameOf(i),
-        partitionsVector->childAt(i)->type()->isDate(),
-        nullValueName));
+        partitionsVector->childAt(i)->type()->isDate()));
   }
   return partitionKeyValues;
 }
