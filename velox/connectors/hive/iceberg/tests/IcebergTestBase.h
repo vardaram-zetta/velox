53c1c1ff4993d6efc581d827f16363a0039bf0a3
Ping Liu <lpingbj@cn.ibm.com>
2025-09-01 16:25:56 +0100

    Add clustered Iceberg writer mode.
    Alchemy-item: (ID = 799) Iceberg staging hub commit 11/11 - 50cc3779a1e3ed4fe97d76611a8d8357797af9f9

--- velox/connectors/hive/iceberg/tests/IcebergTestBase.h
+++ velox/connectors/hive/iceberg/tests/IcebergTestBase.h
@@ -63,9 +63,12 @@ class IcebergTestBase : public exec::test::HiveConnectorTestBase {
     const std::vector<PartitionField>& transformSpecs,
     const RowTypePtr& rowType);
 
+  void setupMemoryPools();
+
   dwio::common::FileFormat fileFormat_{dwio::common::FileFormat::PARQUET};
   RowTypePtr rowType_;
   std::shared_ptr<memory::MemoryPool> opPool_;
+  std::shared_ptr<config::ConfigBase> connectorSessionProperties_;
 
 private:
   IcebergInsertTableHandlePtr createIcebergInsertTableHandle(
@@ -77,10 +80,8 @@ private:
   std::vector<std::string> listPartitionDirectories(
       const std::string& dataPath);
 
-  void setupMemoryPools();
   std::shared_ptr<memory::MemoryPool> root_;
   std::shared_ptr<memory::MemoryPool> connectorPool_;
-  std::shared_ptr<config::ConfigBase> connectorSessionProperties_;
   std::shared_ptr<HiveConfig> connectorConfig_;
   std::unique_ptr<ConnectorQueryCtx> connectorQueryCtx_;
   VectorFuzzer::Options fuzzerOptions_;
