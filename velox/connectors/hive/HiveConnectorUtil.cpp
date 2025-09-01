e30a85be387beba437f70281b2dea7f08db0b12b
Ping Liu <lpingbj@cn.ibm.com>
2025-09-01 16:25:56 +0100

    Add clustered Iceberg writer mode.
    Alchemy-item: (ID = 955) Iceberg staging hub commit 16/16 - ae84c8f1ecb3d0525fc155ac2612c70791227048

--- velox/connectors/hive/HiveConnectorUtil.cpp
+++ velox/connectors/hive/HiveConnectorUtil.cpp
@@ -811,7 +811,6 @@ std::unique_ptr<dwio::common::BufferedInput> createBufferedInput(
 }
 
 namespace {
-
 core::CallTypedExprPtr replaceInputs(
     const core::CallTypedExpr* call,
     std::vector<core::TypedExprPtr>&& inputs) {
@@ -991,12 +990,11 @@ core::TypedExprPtr extractFiltersFromRemainingFilter(
 
   return expr;
 }
+} // namespace
 
 std::string makeUuid() {
   return boost::lexical_cast<std::string>(boost::uuids::random_generator()());
 }
-
-} // namespace
 
 core::TypedExprPtr extractFiltersFromRemainingFilter(
     const core::TypedExprPtr& expr,
