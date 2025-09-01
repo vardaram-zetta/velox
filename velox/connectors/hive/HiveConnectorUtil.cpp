9db788b657a1a229695e56b05d71d1fc7023734c
Ping Liu <lpingbj@cn.ibm.com>
2025-09-01 16:25:56 +0100

    Add clustered Iceberg writer mode.

--- velox/connectors/hive/HiveConnectorUtil.cpp
+++ velox/connectors/hive/HiveConnectorUtil.cpp
@@ -841,7 +841,6 @@ std::unique_ptr<dwio::common::BufferedInput> createBufferedInput(
 }
 
 namespace {
-
 core::CallTypedExprPtr replaceInputs(
     const core::CallTypedExpr* call,
     std::vector<core::TypedExprPtr>&& inputs) {
@@ -1021,12 +1020,11 @@ core::TypedExprPtr extractFiltersFromRemainingFilter(
 
   return expr;
 }
+} // namespace
 
 std::string makeUuid() {
   return boost::lexical_cast<std::string>(boost::uuids::random_generator()());
 }
-
-} // namespace
 
 core::TypedExprPtr extractFiltersFromRemainingFilter(
     const core::TypedExprPtr& expr,
