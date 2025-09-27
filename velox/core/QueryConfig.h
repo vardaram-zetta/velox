292dcd308ab306ff388bb8f7970bce2df4dd33f8
yingsu00 <yingsu00@outlook.com>
2025-09-27 00:43:28 -0700

    Change integral upcasts to identity projection for join keys

--- velox/core/QueryConfig.h
+++ velox/core/QueryConfig.h
@@ -716,6 +716,9 @@ class QueryConfig {
   static constexpr const char* kRowSizeTrackingEnabled =
       "row_size_tracking_enabled";
 
+  static constexpr const char* kSkipIntegerUpCastsForHashJoin =
+      "skip_integer_upcasts_for_hash_join";
+
   bool selectiveNimbleReaderEnabled() const {
     return get<bool>(kSelectiveNimbleReaderEnabled, false);
   }
@@ -1289,6 +1292,10 @@ class QueryConfig {
 
   std::string clientTags() const {
     return get<std::string>(kClientTags, "");
+  }
+
+  bool skipIntegerUpcastsForHashJoinEnabled() const {
+    return get<bool>(kSkipIntegerUpCastsForHashJoin, false);
   }
 
   template <typename T>
